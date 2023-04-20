16b650d201d84901555ffe739fee9b04ae1f52e6
tiendung1112 <traitre37@gmail.com>
2023-04-20 22:12:20 +0700

    commit

--- main.js
+++ main.js
@@ -1,37 +1,131 @@
-// Tạo hình ảnh panorama
-const panoramaImage = new PANOLENS.ImagePanorama('images/image2.jpg');
+// Yêu cầu quyền truy cập cảm biến
+if (window.DeviceOrientationEvent && typeof navigator.permissions !== "undefined") {
+  navigator.permissions.query({ name: "accelerometer" }).then(function (permissionStatus) {
+    if (permissionStatus.state === "granted") {
+      window.addEventListener("deviceorientation", DeviceOrientationControls);
+    } else if (permissionStatus.state === "prompt") {
+      navigator.permissions
+        .request({ name: "accelerometer" })
+        .then(function (permissionStatus) {
+          if (permissionStatus.state === "granted") {
+            window.addEventListener("deviceorientation", DeviceOrientationControls);
+          }
+        });
+    }
+  });
+}
 
-// Lấy phần tử chứa hình ảnh
-const imageContainer = document.querySelector('.image-container');
+// Xử lý chuyển động
+function handleDeviceOrientation(event) {
+  // Lấy giá trị chuyển động trên trục X và Y
+  const alpha = event.alpha;
+  const beta = event.beta;
 
-// Tạo đối tượng Viewer
+  // Áp dụng chuyển động vào ảnh
+  panoramaImage.rotation.y = (alpha * Math.PI) / 180;
+  panoramaImage.rotation.x = ((beta - 90) * Math.PI) / 180;
+}
+
+function DeviceOrientationControls(object) {
+  const scope = this;
+  this.object = object;
+  this.object.rotation.reorder('YXZ');
+  this.enabled = true;
+  this.deviceOrientation = null;
+  this.screenOrientation = 0;
+  this.alphaOffset = 0; // radians
+  this.initialOffset = null;
+  const onDeviceOrientationChangeEvent = function ({ alpha, beta, gamma }) {
+    if (scope.initialOffset === null) {
+      scope.initialOffset = alpha;
+    }
+    alpha = alpha - scope.initialOffset;
+    if (alpha < 0) alpha += 360;
+    scope.deviceOrientation = { alpha, beta, gamma };
+  };
+  const onScreenOrientationChangeEvent = function () {
+    scope.screenOrientation = window.orientation || 0;
+  };
+  const onRegisterEvent = function () {
+    window.addEventListener('orientationchange', onScreenOrientationChangeEvent, false);
+    window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
+  }.bind(this);
+  // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
+  const setObjectQuaternion = function () {
+    const zee = new THREE.Vector3(0, 0, 1);
+    const euler = new THREE.Euler();
+    const q0 = new THREE.Quaternion();
+    const q1 = new THREE.Quaternion(- Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
+    return function (quaternion, alpha, beta, gamma, orient) {
+      euler.set(beta, alpha, - gamma, 'YXZ'); // 'ZXY' for the device, but 'YXZ' for us
+      quaternion.setFromEuler(euler); // orient the device
+      quaternion.multiply(q1); // camera looks out the back of the device, not the top
+      quaternion.multiply(q0.setFromAxisAngle(zee, - orient)); // adjust for screen orientation
+    };
+  }();
+  this.connect = function () {
+    onScreenOrientationChangeEvent(); // run once on load
+    // iOS 13+
+    if (window.DeviceOrientationEvent !== undefined && typeof window.DeviceOrientationEvent.requestPermission === 'function') {
+      window.DeviceOrientationEvent.requestPermission().then(function (response) {
+        if (response == 'granted') {
+          onRegisterEvent();
+        }
+      }).catch(function (error) {
+        console.error('THREE.DeviceOrientationControls: Unable to use DeviceOrientation API:', error);
+      });
+    } else {
+      onRegisterEvent();
+    }
+    scope.enabled = true;
+  };
+  this.disconnect = function () {
+    window.removeEventListener('orientationchange', onScreenOrientationChangeEvent, false);
+    window.removeEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
+    scope.enabled = false;
+    scope.deviceOrientation = null;
+    scope.initialOffset = null;
+
+  };
+  this.update = function ({ theta = 0 } = { theta: 0 }) {
+    if (scope.enabled === false) return;
+    const device = scope.deviceOrientation;
+    if (device) {
+      const alpha = device.alpha ? THREE.Math.degToRad(device.alpha) + scope.alphaOffset : 0; // Z
+
+      const beta = device.beta ? THREE.Math.degToRad(device.beta) : 0; // X'
+      const gamma = device.gamma ? THREE.Math.degToRad(device.gamma) : 0; // Y''
+      const orient = scope.screenOrientation ? THREE.Math.degToRad(scope.screenOrientation) : 0; // O
+      setObjectQuaternion(scope.object.quaternion, alpha + theta, beta, gamma, orient);
+    }
+  };
+  this.dispose = function () {
+    scope.disconnect();
+  };
+  this.getAlpha = function () {
+    const { deviceOrientation: device } = scope;
+    return device && device.alpha ? THREE.Math.degToRad(device.alpha) + scope.alphaOffset : 0;
+  };
+  this.getBeta = function () {
+    const { deviceOrientation: device } = scope;
+    return device && device.beta ? THREE.Math.degToRad(device.beta) : 0;
+  };
+};
+DeviceOrientationControls.prototype = Object.assign(Object.create(THREE.EventDispatcher.prototype), {
+  constructor: DeviceOrientationControls
+});
+
+// Khởi tạo Panorama
+const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+
+// Khởi tạo Viewer
+const imageContainer = document.querySelector(".image-container");
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
   autoRotate: true,
   autoRotateSpeed: 0.3,
-  controlBar: false
+  controlBar: false,
 });
 
-// Thêm hình ảnh panorama vào viewer
+// Thêm Panorama vào Viewer
 viewer.add(panoramaImage);
-
-// Xin quyền truy cập đến cảm biến định hướng trên thiết bị
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', handleOrientation);
-}
-
-// Hàm xử lý sự kiện cập nhật từ cảm biến định hướng
-function handleOrientation(event) {
-  // Lấy giá trị alpha, beta và gamma từ sự kiện cập nhật
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
-
-  // Tính toán góc xoay theo trục x, y và z của hình ảnh panorama
-  const x = (beta - 90) * Math.PI / 180;
-  const y = -gamma * Math.PI / 180;
-  const z = (alpha + 180) * Math.PI / 180;
-
-  // Cập nhật góc xoay của hình ảnh panorama
-  panoramaImage.rotation.set(x, y, z);
-}
