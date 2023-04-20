a57391dde086f2f0e57b9b96866d0c5e7ca9b43e
tiendung1112 <traitre37@gmail.com>
2023-04-20 21:27:12 +0700

    commit

--- main.js
+++ main.js
@@ -1,73 +1,35 @@
-// Khởi tạo panoramaImage
-const panoramaImage = new PANOLENS.ImagePanorama("images/image2.jpg");
+// Lấy DOM container và khởi tạo Panolens Viewer
+const container = document.querySelector('.image-container');
+const viewer = new PANOLENS.Viewer({ container });
 
-// Lấy container để chứa ảnh
-const imageContainer = document.querySelector(".image-container");
+// Khởi tạo ảnh panorama
+const panorama = new PANOLENS.ImagePanorama('images/image2.jpg');
+viewer.add(panorama);
 
-// Khởi tạo viewer và thêm panoramaImage vào viewer
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  autoRotate: true,
-  autoRotateSpeed: 0.3,
-  controlBar: false,
-});
-viewer.add(panoramaImage);
-
+// Kiểm tra nếu trình duyệt hỗ trợ cảm biến
 if (window.DeviceOrientationEvent) {
-  // Yêu cầu quyền truy cập vào cảm biến
-  navigator.permissions.query({ name: 'accelerometer' }).then(result => {
-    if (result.state === 'granted') {
-      console.log('Quyền truy cập cảm biến đã được cấp');
-      // Bắt đầu sử dụng sự kiện "deviceorientation"
-    } else if (result.state === 'prompt') {
-      console.log('Người dùng sẽ xác nhận quyền truy cập cảm biến');
-      // Yêu cầu người dùng xác nhận quyền truy cập cảm biến
-      navigator.permissions.request({ name: 'accelerometer' }).then(result => {
-        if (result.state === 'granted') {
-          console.log('Quyền truy cập cảm biến đã được cấp');
-          // Bắt đầu sử dụng sự kiện "deviceorientation"
-        } else {
-          console.log('Quyền truy cập cảm biến bị từ chối');
-        }
-      });
-    } else {
-      console.log('Quyền truy cập cảm biến bị từ chối');
+  // Xác định hướng thiết bị
+  let alpha = 0, beta = 0, gamma = 0;
+  window.addEventListener('deviceorientation', (event) => {
+    alpha = event.alpha;
+    beta = event.beta;
+    gamma = event.gamma;
+  });
+
+  // Xử lý sự kiện
+  viewer.addEventListener('animate', () => {
+    // Nếu cảm biến không hoạt động thì không làm gì cả
+    if (!alpha || !beta || !gamma) {
+      return;
+    }
+
+    // Tính toán góc quay và xoay ảnh
+    const angle = (beta / 180) * Math.PI;
+    panorama.rotation.y = angle;
+
+    // Lật ảnh nếu thiết bị nằm ngang
+    if (Math.abs(gamma) < 10) {
+      panorama.rotation.y += Math.PI;
     }
   });
-} else {
-  console.log('Thiết bị của bạn không hỗ trợ sự kiện "deviceorientation"');
-}
-
-// Khởi tạo các biến để lưu trữ góc quay của điện thoại
-let gamma = 0;
-let beta = 0;
-
-// Đăng ký hàm xử lý sự kiện "deviceorientation"
-window.addEventListener("deviceorientation", handleOrientation);
-
-function handleOrientation(event) {
-  // Lấy giá trị gamma và beta từ event
-  const newGamma = event.gamma;
-  const newBeta = event.beta;
-
-  // Tính toán sự khác biệt giữa gamma và beta hiện tại và giá trị mới
-  const gammaDiff = Math.abs(gamma - newGamma);
-  const betaDiff = Math.abs(beta - newBeta);
-
-  // Đặt ngưỡng để kiểm tra sự khác biệt
-  const gammaThreshold = 2;
-  const betaThreshold = 2;
-
-  // Nếu sự khác biệt lớn hơn ngưỡng, thì cập nhật gamma và beta mới và thực hiện di chuyển ảnh
-  if (gammaDiff > gammaThreshold || betaDiff > betaThreshold) {
-    gamma = newGamma;
-    beta = newBeta;
-    requestAnimationFrame(() => {
-      // Tính toán góc quay mới của ảnh dựa trên các giá trị gamma và beta mới
-      const rotationY = THREE.Math.degToRad(beta);
-      const rotationX = THREE.Math.degToRad(-gamma);
-      const quaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(rotationY, rotationX, 0, "YZX"));
-      panoramaImage.setRotationFromQuaternion(quaternion);
-    });
-  }
 }
